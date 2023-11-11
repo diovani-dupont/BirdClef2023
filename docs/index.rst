0b7ffe31cf61c8e3ac3cc0549cd8a502922b79bb
Bowie <marketing@letscode.com.br>
2023-11-11 18:38:18 -0300

    First commit

---
+++ docs/index.rst
@@ -0,0 +1,25 @@
+.. BirdCLEF documentation master file, created by
+   sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+BirdCLEF documentation!
+==============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   getting-started
+   commands
+   modules
+   ../apidoc/modules.rst
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The following code is a script developed for Kaggle's BirdCLEEF 2023 competition, it is a machine learning script that processes, trains and evaluates an audio classification model, based on features extracted from audio files. It uses the RandomForestClassifier algorithm to train a model that identifies birdsong in audio recordings.',
+    author='Diovani Dupont',
+    license='',
+)
